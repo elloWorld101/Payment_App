04f280dd701b79c41b074867a24e0453a431c200
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-10 15:44:26 +0530

    Update Landing.jsx

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Loading } from '../components/Loading';
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 
 //There is no JSX but I have still created the Landing component and not only a js file as I can use the useNavigate hook inside BrowserRouter --> correct me If I am wrong
 export function Landing(){
+    const [loading, setLoading] = useState(true);
     const token = localStorage.getItem('jwtToken');
     const navigate = useNavigate();
 
@@ -17,6 +19,7 @@ export function Landing(){
         })
         .then(function(response){
             const data = response.data.msg;
+            setLoading(false);
             if(data == "Navigate to signin"){
                 navigate("/dashboard");
             }else{
@@ -28,6 +31,7 @@ export function Landing(){
 
     return(
         <> 
+            {loading ? <Loading/> : null} 
         </>
     )
 }
