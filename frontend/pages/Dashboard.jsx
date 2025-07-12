370101a4d23f6f16b9f567d12c6530602c31aade
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-12 16:58:44 +0530

    Update Dashboard.jsx

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -105,17 +105,17 @@ export function Dashboard(){
 
                 <h2 class="lg:text-2xl font-semibold lg:p-0 text-md">Payments App</h2>
 
-                {userSkeleton? <UserSkeleton/> : <div 
-                class="font-medium pl-3 lg:p-0 text-md"> 
-                    <p>Hello, {user.name} <button //onClick={update}
-                    class="lg:px-3 lg:ml-2 lg:text-lg lg:rounded-full lg:bg-teal-800 lg:text-white lg:font-semibold lg:p-1 group relative
-                            text-md rounded-full bg-teal-800 text-white px-2.5 p-1 ml-1">
-                        {user.box}
-                        <div class="absolute top-full text-black right-3 bg-white rounded-md p-2 text-sm mt-1 shadow-md scale-y-0 group-focus:scale-y-100 origin-top duration-200">
-                            <a class="active" onClick={update}>Update</a>
-                        </div>
-                        </button></p>
-                </div>}
+                    {userSkeleton? <UserSkeleton/> : <div 
+                    class="font-medium pl-3 lg:p-0 text-md">
+                        <p>Hello, {user.name} <button //onClick={update}
+                        class="lg:px-3 lg:ml-2 lg:text-lg lg:rounded-full lg:bg-teal-800 lg:text-white lg:font-semibold lg:p-1 group relative
+                                text-md rounded-full bg-teal-800 text-white px-2.5 p-1 ml-1">
+                            {user.box}
+                            <div class="absolute text-sm top-full text-black right-3 bg-white rounded-md p-2 shadow-md scale-y-0 group-focus:scale-y-100 origin-top duration-200">
+                                <a class="active" onClick={update}>Update</a>
+                            </div>
+                            </button></p>
+                    </div>}
                 
             </div>
 
