a5bedd6649c27827615c947a5949584b1d1d539d
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-12 11:50:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import { Signin } from '../pages/Signin'
 import { Signup } from '../pages/Signup'
 import { Dashboard } from '../pages/Dashboard'
@@ -12,6 +13,7 @@ function App() {
   return (
       <>
         <BrowserRouter>
+        <RecoilRoot>
           <Routes>
             <Route path='/' element={<Landing/>}></Route>
             <Route path='/signup' element={<Signup/>}></Route>
@@ -20,6 +22,7 @@ function App() {
             <Route path='/send' element={<SendMoney/>}></Route>
             <Route path='/update' element={<Update/>}></Route>
           </Routes>
+        </RecoilRoot>
         </BrowserRouter>
       </>
   )
