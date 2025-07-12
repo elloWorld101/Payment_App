f7b12e20f545fdd89f1112877691b9e9697f2143
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-12 14:27:53 +0530

    Update UserSkeleton.jsx

--- frontend/components/UserSkeleton.jsx
+++ frontend/components/UserSkeleton.jsx
@@ -1,13 +1,14 @@
 export function UserSkeleton(){
     return(
         <div>
-            <div className="w-40 ">
-                <div className="flex flex-row items-center  justify-end gap-4">
-                    
-                    <div className="flex-1 space-y-2">
-                        <div className="h-4 bg-gray-300 rounded w-4/4 "></div>
-                    </div>
-                    <div className="w-12 h-12 rounded-full bg-gray-300"></div>
+            <div className="lg:w-40 w-25">
+                <div className="flex flex-row items-center  justify-end lg:gap-4 gap-2">
+                        <div className="flex-1 space-y-2">
+                            <div className="h-4 bg-gray-300 rounded lg:w-4/4 "></div>
+                        </div>
+                    <div className="lg:w-12 lg:h-12 rounded-full bg-gray-300
+                        w-8 h-8
+                    "></div>
                 </div>    
             </div>
         </div>
