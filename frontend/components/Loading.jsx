4911597440c33f6b7e66c9dd217d7763db9de35f
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-13 12:33:47 +0530

    Update Loading.jsx

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -2,9 +2,10 @@
 export function Loading(){
     return(
         <div>
-            <div className="flex justify-center h-screen items-center">
+            <div className="flex flex-col justify-center h-screen items-center ">
+                <h2>Please wait, I did not had the money to buy a fast server</h2>
                 <div
-                    className="animate-spin inline-block w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full"
+                    className="animate-spin m-4 inline-block w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full"
                     role="status"
                     aria-label="loading"
                 >
