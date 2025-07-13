f3236276767c63127573e6f33939466f5e67c615
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-13 12:38:25 +0530

    Update Loading.jsx

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -3,7 +3,7 @@ export function Loading(){
     return(
         <div>
             <div className="flex flex-col justify-center h-screen items-center ">
-                <h2>Please wait, I did not had the money to buy a fast server</h2>
+                <h2 class="w-[90%] text-center">Please wait, I do not hve the money to buy a fast server</h2>
                 <div
                     className="animate-spin m-4 inline-block w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full"
                     role="status"
