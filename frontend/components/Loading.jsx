764778fb12b008cb57ac118483579685e5743354
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-14 16:47:04 +0530

    Update Loading.jsx

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -1,9 +1,20 @@
+import {useState} from 'react'
 
 export function Loading(){
+    const [heading, setHeading] = useState(false);
+    
+    function setter(){
+        setTimeout(()=>{
+            setHeading(true);
+        },5000)
+    }
+   
     return(
         <div>
             <div className="flex flex-col justify-center h-screen items-center ">
-                <h2 class="w-[90%] text-center">Please wait, I do not hve the money to buy a fast server</h2>
+                {setter()}                
+                {heading ? <h2 class="w-[90%] text-center">Bhai rukjao, website dekhke jana</h2> : null }
+
                 <div
                     className="animate-spin m-4 inline-block w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full"
                     role="status"
